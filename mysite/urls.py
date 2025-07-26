381faf8edd5730db39d9a7118fd7eaaab5df3257
Uma Kulkarni <umask1234@gmail.com>
2025-07-25 22:11:50 -0400

    Bookshelf added.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("annotation/", include("annotation.urls")),
+    path("bookshelf/", include("bookshelf.urls")),
     path('admin/', admin.site.urls),
 ]
